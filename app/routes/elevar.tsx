6a9f5a4e5430535fe126e8bdab6a7ecc2faff3c7
Kieran Reynolds <kieran@sparky.us>
2023-10-09 14:32:52 -0700

    updates elevar copy

--- app/routes/elevar.tsx
+++ app/routes/elevar.tsx
@@ -16,7 +16,7 @@ export default function ElevarPage() {
         textVerticalPosition='middle'
       >
         <h2>CRO Readiness Checklist</h2>
-        <p className='h4 leading-8 font-normal'>Send us your site and we’ll give you 3 brand-specific ideas to level up this holiday season.</p>
+        <p className='h4 leading-8 font-normal'>Looking to improve conversion rate? Before you start testing, download this checklist to determine site readiness and avoid common pitfalls that come in the way growth.</p>
       </ContentArea>
       <ElevarSubmissionSimpleForm />
       <ContentArea
